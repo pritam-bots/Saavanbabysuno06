0c13876d7918e0af5b671084b9fdf6476187cd76
mrarya-bit <mrsunny2136@gmail.com>
2025-08-10 11:07:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/mraryasingh/Babumusic01",
+    "https://github.com/mrarya-bit/Youtube2.0baby05",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
